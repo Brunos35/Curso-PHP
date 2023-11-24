dbbf4713ddd6e81e044c89f8594d298c73b8cc7d
Brunos35 <87710790+Brunos35@users.noreply.github.com>
2023-11-24 15:50:49 -0300

    desafio concluido

--- desafio01/calc.php
+++ desafio01/calc.php
@@ -1,5 +1,23 @@
-<?php 
-    $num = $_POST['num'];
-    echo "O Sucessor é ($num+1)";
-    echo "O antecessor é ($num-1)";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Resultado</h1>
+    </header>
+    <section>
+        <?php
+        $num = $_POST['num'];
+        $suc = ($num+1);
+        $ant = ($num-1);
+        echo "O Sucessor é $suc <br>";
+        echo "O Antecessor é $ant";
+        ?>
+    </section>
+</body>
+</html>
